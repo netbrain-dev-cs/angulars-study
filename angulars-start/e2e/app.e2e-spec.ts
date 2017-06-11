8dc02e70b354f1828110c6a289c6aa8a54d6aeb4
netbrain-cs <l_m_h1027@163.com>
2017-06-11 14:25:13 +0800

    change relative tests code

--- angulars-start/e2e/app.e2e-spec.ts
+++ angulars-start/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angulars-start App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual(`Welcome to my nb-cs's app!!`);
   });
 });
