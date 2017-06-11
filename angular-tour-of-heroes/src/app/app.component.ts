3ed218122d06a6ce71361fc75231b5ecabe0ed36
576566385@qq.com <576566385@qq.com>
2017-06-11 20:46:19 +0800

    add click handler for hero list

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -4,6 +4,7 @@ export class Hero{
   id:number;
   name:string;
 }
+
 const HEROES:Hero[]=[
   {id:11,name:'Mr.Nice'},
   {id:12,name:'Narco'},
@@ -23,16 +24,18 @@ const HEROES:Hero[]=[
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+    <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
       <span class="badge">{{hero.id}}</span>{{hero.name}}
       <!-- each hero goes here -->
     </li>
   </ul>
-  <h2>{{hero.name}} details</h2>
-  <div><label>id:</label>{{hero.id}}</div>
-  <div>
-    <label>name:</label>
-    <input [(ngModel)]="hero.name" placeholder="name">
+  <div *ngIf="selectedHero">
+    <h2>{{selectedHero.name}} details</h2>
+    <div><label>id:</label>{{selectedHero.id}}</div>
+    <div>
+      <label>name:</label>
+      <input [(ngModel)]="selectedHero.name" placeholder="name">
+    </div>
   </div>
   `,
   styles:[`
@@ -80,9 +83,10 @@ const HEROES:Hero[]=[
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero:Hero = {
-    id:1,
-    name:'windstorm'
-  };
   heroes = HEROES;
+  selectedHero: Hero; 
+
+  onSelect(hero:Hero):void{
+    this.selectedHero = hero;
+  }
 }
