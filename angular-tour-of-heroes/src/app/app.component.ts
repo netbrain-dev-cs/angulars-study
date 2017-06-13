70d9ae92f3f7b50d2675e98a994697e1d739b76b
netbrain-cs <l_m_h1027@163.com>
2017-06-13 13:26:20 +0800

    add router:'heroes'

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     selector: 'app-root',
     template:`
         <h1>{{title}}</h1>
-        <my-heroes></my-heroes>
+        <router-outlet></router-outlet>
     `
 })
 
