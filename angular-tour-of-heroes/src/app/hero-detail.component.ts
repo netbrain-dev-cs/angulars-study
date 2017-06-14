bee8a70d67fb8480dcac07bd8d40956c20f8c78c
netbrain-cs <l_m_h1027@163.com>
2017-06-14 16:46:02 +0800

    add http service

--- angular-tour-of-heroes/src/app/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail.component.ts
@@ -1,36 +1,37 @@
-import {Component,Input,OnInit} from '@angular/core';
-import {ActivatedRoute,Params} from '@angular/router';
-import { Location }             from '@angular/common';
-
-import { HeroService} from './hero.service';
-import {Hero} from './hero';
-
 import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
 
-
+import { Hero }        from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
-    selector:'hero-detail',
-    templateUrl:'./hero-detail.component.html',
-    styleUrls:['./hero-detail.component.css']
+  selector: 'hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.css' ]
 })
-export class HeroDetailComponent implements OnInit{
-    constructor(
-        private heroService:HeroService,
-        private route:ActivatedRoute,
-        private location:Location
-    ){}
-    
-    ngOnInit():void{
-        this.route.params
-        .switchMap((params:Params)=>this.heroService.getHero(+params['id']))
-        .subscribe(hero=>this.hero=hero);
-    }
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
 
-    goBack():void{
-        this.location.back();
-    }
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
-    @Input() 
-    hero:Hero;
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
