d32556688a0af5043d8db56bb4dc82f915b918ed
netbrain-cs <l_m_h1027@163.com>
2017-06-14 14:36:33 +0800

    add style files
    add .css file,beauti the hero

--- angular-tour-of-heroes/src/app/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes.component.ts
@@ -6,23 +6,7 @@ import {HeroService} from './hero.service';
 
 @Component({
   selector: 'my-heroes',
-  template:`  
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li *ngFor="let hero of heroes"     
-      [class.selected]="hero === selectedHero"
-      (click)="onSelect(hero)" >
-      <span class="badge">{{hero.id}}</span>{{hero.name}}
-      <!-- each hero goes here -->
-    </li>
-  </ul>
-  <div *ngIf="selectedHero">
-    <h2>
-      {{selectedHero.name | uppercase}} is my hero
-    </h2>
-    <button (click)="gotoDetail()">View Detail</button>
-  `,
-  //templateUrl:'./app.component.html',
+  templateUrl:'./heroes.component.html',
   styleUrls:['./heroes.component.css'],
   providers:[]
 })
